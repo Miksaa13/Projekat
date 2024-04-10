5d58ed93953cbcafa1cdfdd534a43c386d0344e5
matf-racunarska-grafika <t.mihajlo20@gmail.com>
2024-04-10 22:29:38 +0200

    rebull

--- src/main.cpp
+++ src/main.cpp
@@ -67,9 +67,11 @@ struct ProgramState {
     glm::vec3 backpackPosition = glm::vec3(-1.0f,0.0f,0.0f);
     glm::vec3 stazaPosition = glm::vec3(-2.7f,2.1f,0.0f);
     glm::vec3 mercPosition = glm::vec3(-2.7f,0.2f,-5.0f);
+    glm::vec3 redbullPosition = glm::vec3(2.1f,0.2f,-10.0f);
     float backpackScale = 1.0f;
     float stazaScale = 0.6f;
     float mercScale = 1.1f;
+    float redbullScale = 0.45f;
     PointLight pointLight;
     DirLight dirlight;
     ProgramState()
@@ -258,6 +260,9 @@ int main() {
     Model stazaModel("resources/objects/staza/untitled.obj");
     stazaModel.SetShaderTextureNamePrefix("material.");
 
+    Model redbullModel("resources/objects/redbull/untitled.obj");
+    redbullModel.SetShaderTextureNamePrefix("material.");
+
     //stbi_set_flip_vertically_on_load(true);
 
 
@@ -352,6 +357,13 @@ int main() {
         model = glm::scale(model, glm::vec3(programState->mercScale));
         ourShader.setMat4("model", model);
         mercModel.Draw(ourShader);
+        model = glm::mat4(1.0f);
+
+        model = glm::translate(model,programState->redbullPosition);
+        model = glm::rotate(model,(float)235.4,glm::vec3(0.0f,1.0f,0.01f));
+        model = glm::scale(model, glm::vec3(programState->redbullScale));
+        ourShader.setMat4("model", model);
+        redbullModel.Draw(ourShader);
         model = glm::mat4(1.0f);
 
         //drawing skybox
